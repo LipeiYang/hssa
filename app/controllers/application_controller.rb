1042ac266407ed90975d7bd0123db41d509f4d00
Lipei Yang <lipei.yang@gmail.com>
2010-07-28 00:06:45 +0800

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
+  before_filter :hijack_ie_default_format
+  def hijack_ie_default_format
+    if request.user_agent =~ /MSIE/ and params['format'].nil?
+      params['format'] = 'html'
+    end
+  end
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
